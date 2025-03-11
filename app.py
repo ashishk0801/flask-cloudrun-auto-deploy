80f15746d52df427098984b4f7ec155cc02dd066
Ashish K <ashishl2421@gmail.com>
2025-03-11 14:28:38 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from udupi'
+    return 'Hello from the other side'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
